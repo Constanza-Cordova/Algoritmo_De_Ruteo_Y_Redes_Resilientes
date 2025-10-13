1beeadb0f9e52fab372e9ea82fe618a41ac0d75e
Constanza-Cordova <81639034+Constanza-Cordova@users.noreply.github.com>
2025-10-12 21:15:45 -0300

    Create cargar_amenaza_3.py

---
+++ Carga BD/cargar_amenaza_3.py
@@ -0,0 +1,22 @@
+import json
+import psycopg2
+
+def cargar_amenazas():
+    conn = psycopg2.connect(dbname="tu_bd", user="usuario", password="clave", host="localhost")
+    cur = conn.cursor()
+
+    with open("Amenazas/amenaza_3.json") as f:
+        amenazas = json.load(f)
+
+    for amenaza in amenazas:
+        cur.execute("""
+            INSERT INTO amenaza (id, nodo_id, descripcion, nivel_riesgo)
+            VALUES (%s, %s, %s, %s)
+        """, (amenaza["id"], amenaza["nodo_id"], amenaza["descripcion"], amenaza["nivel_riesgo"]))
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+if __name__ == "__main__":
+    cargar_amenazas()
